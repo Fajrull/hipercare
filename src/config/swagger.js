3796ebe5c866bf27ecada58582fca7e2252b1e93
Fajrull <fajrulm05@gmail.com>
2026-07-14 11:04:56 +0700

    chore : update swagger

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -11,8 +11,8 @@ const options = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 3000}`,
-        description: 'Development Server',
+        url: 'https://kimerahealth.site',
+        description: 'Production Server',
       },
     ],
     components: {
